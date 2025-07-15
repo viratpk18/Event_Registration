f18fac8fbb5b79ed94eaebbd2faea4cb7f2fa83f
viratpk18 <praveenkumar2k4.t@gmail.com>
2025-07-15 21:45:19 +0530

    backend files created sucessfully

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/events", require("./routes/event"));
+app.use("/api/register", require("./routes/registration"));
+app.use("/api/admin", require("./routes/admin"));
+
+
+app.get("/", (req, res) => {
+  res.send("EventEase Backend Running ✅");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
